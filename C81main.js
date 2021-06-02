c2e8d1243f58349038440c9ac14ceb8245b9965b
killercosmos <76935007+killercosmos@users.noreply.github.com>
2021-06-02 19:33:17 +0530

    Update C81main.js

--- C81main.js
+++ C81main.js
@@ -1,8 +1,8 @@
 canvas = document.getElementById("mycanvas");
-var ctx = canvas.getcontext("2d");
+var ctx = canvas.getContext("2d");
 color="red";
 
-ctx.beginpath();
+ctx.beginPath();
 ctx.strokestyle = color;
 ctx.linewidth = 2;
 ctx.arc(200,200,40,0,2*Math.PI);
@@ -17,7 +17,7 @@ function my_mousedown(e)
     mouse_x = e.clientX - canvas.offsetLeft;
     mouse_y = e.clientY - canvas.offsetTop;
     console.log("x -" + mouse_x + "y -" + mouse_y);
-    circle(muse_x,mouse_y);
+    circle(mouse_x,mouse_y);
 }
 
 function circle(mouse_x,mouse_y)
